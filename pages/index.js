188a1bb16527b82977d5aed4e5938188958714a8
VenkataNagaAparnaGavini94 <136665500+VenkataNagaAparnaGavini94@users.noreply.github.com>
2023-08-05 20:38:10 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hello Professor</title>
+      </Head>
+      <h1>Hello, Professor!</h1>
+    </div>
+  );
+}
